acddde4c3ee907ac5d3befbdcad1800981f04c2c
jordan <leesoyoung0112@gmail.com>
2022-09-15 13:58:55 +0900

    2022-09-15

--- calculator/app/src/main/java/kr/ac/dukdung/calculator/MainActivity.java
+++ calculator/app/src/main/java/kr/ac/dukdung/calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
@@ -27,14 +28,20 @@ public class MainActivity extends AppCompatActivity {
         btnAdd = (Button) findViewById(R.id.BtnAdd);
         btnSub = (Button) findViewById(R.id.BtnSub);
         textResult = (TextView) findViewById(R.id.TextResult);
-
         btnAdd.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View arg0, MotionEvent arg1) {
                 num1 = edit1.getText().toString();
                 num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) + Integer.parseInt(num2);
-                textResult.setText("계산 결과: " + result.toString());
+
+                if(num1.equals("")) {
+                    textResult.setText("숫자1을 입력하세요");
+                } else if(num2.equals("")) {
+                    textResult.setText("숫자2를 입력하세요");
+                } else {
+                    result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                    textResult.setText("계산 결과: " + result.toString());
+                }
                 return false;
             }
         });
@@ -44,8 +51,15 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View arg0, MotionEvent arg1) {
                 num1 = edit1.getText().toString();
                 num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) - Integer.parseInt(num2);
-                textResult.setText("계산 결과: " + result.toString());
+
+                if(num1.equals("")) {
+                    textResult.setText("숫자1을 입력하세요");
+                } else if(num2.equals("")) {
+                    textResult.setText("숫자2를 입력하세요");
+                } else {
+                    result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                    textResult.setText("계산 결과: " + result.toString());
+                }
                 return false;
             }
         });
